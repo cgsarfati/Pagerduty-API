ec4fc2b29a99c63e719c8ab94f02b91f388c7655
vagrant <vagrant@vagrant.vm>
2017-12-09 01:36:34 +0000

    finished contact method fn.

--- create_users.py
+++ create_users.py
@@ -17,6 +17,7 @@ base_url = 'https://api.pagerduty.com/users'
 
 
 def create_user(headers, email, payload):
+    """Creates user. Returns user info."""
 
     # Add admin's email, NOT the user being added
     headers['From'] = email
@@ -25,8 +26,19 @@ def create_user(headers, email, payload):
     r = requests.post(base_url, headers=headers, data=json.dumps(payload))
 
     print 'User creation response code: ' + str(r.status_code)
-
     return r.json()['user']
+
+
+def create_contact_method(headers, user_id, payload):
+    """Creates contact method of user. Returns id."""
+
+    # Alter base_url's endpoint
+    url = base_url + '/' + user_id + '/contact_methods'
+
+    r = requests.post(url, headers=headers, data=json.dumps(payload))
+
+    print 'Contact method response code: ' + str(r.status_code)
+    return r.json()['contact_method']['id']
 
 
 def process_csv(headers, email):
@@ -42,11 +54,40 @@ def process_csv(headers, email):
             'name': row['name'],
             'email': row['email'],
             'role': row['role'],
-            'type': 'user',
-            'summary': row['name']
+            'type': 'user',  # optional
             }
 
-    r = create_user(headers, email, user)
+        r = create_user(headers, email, user)
+        user_id = r['id']
+
+        if row['type'] == 'phone':
+            contact_method = {
+                'contact_method': {
+                    'type': 'phone_contact_method',
+                    'label': 'Mobile',
+                    'address': row['address']
+                    }
+                }
+
+        elif row['type'] == 'sms':
+            contact_method = {
+                'contact_method': {
+                    'type': 'sms_contact_method',
+                    'label': 'Mobile',
+                    'address': row['address']
+                    }
+                }
+
+
+        contact_method_id = create_contact_method(headers, user_id, contact_method)
+
+        notification_rule = {
+            'notification_rule': {
+                
+            }
+        }
+
+
 
 
 
